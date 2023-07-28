888fba20cee0d855f73e0dbfc97a1f98eabf1c5c
nahidfarazi <nahidfarazi19@gmail.com>
2023-07-28 14:43:46 +0600

    registrations

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
     def index 
-        flash.now[:notice] = "Logged in successfully"
-        flash.now[:alert] = "invalid email or password"
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+        end   
     end
 end  
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,8 +3,9 @@ class RegistrationsController < ApplicationController
         @user = User.new
     end
     def create 
-        @user = User.new(user_params)
+        @user = User.new(user_params) 
         if @user.save 
+            session[:user_id] = @user.id
             redirect_to root_path, notice: "account created successfully"
         else
             render :new 
